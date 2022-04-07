485529097e609f42f46a874509ae6f264df41db7
jeffin997 <79822558+jeffin997@users.noreply.github.com>
2022-04-07 15:51:26 +0530

    Add files via upload

---
+++ program1.sh
@@ -0,0 +1,6 @@
+echo Current date and time is
+$read date
+echo Current directory is
+$read pwd
+echo Current Username is $USER
+
---
+++ program2.sh
@@ -0,0 +1,6 @@
+echo Enter the first Number:
+read a
+echo Enter the Second Number:
+read b
+echo The sum of number is: $sum
+sum= expr $a + $b
